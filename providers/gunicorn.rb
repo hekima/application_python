d4cb666732dcdc7795f3e12d1c4dc0e3cbaee8b2
Thiago Cardoso <thiagoncc@gmail.com>
2014-12-12 14:33:33 -0200

    removing special treatment for django apps

--- providers/gunicorn.rb
+++ providers/gunicorn.rb
@@ -81,14 +81,8 @@ action :before_deploy do
     if new_resource.environment
       environment new_resource.environment
     end
-    if new_resource.app_module == :django
-      django_resource = new_resource.application.sub_resources.select{|res| res.type == :django}.first
-      raise "No Django deployment resource found" unless django_resource
-      base_command = "#{::File.join(django_resource.virtualenv, "bin", "python")} manage.py run_gunicorn"
-    else
-      gunicorn_command = new_resource.virtualenv.nil? ? "gunicorn" : "#{::File.join(new_resource.virtualenv, "bin", "gunicorn")}"
-      base_command = "#{gunicorn_command} #{new_resource.app_module}"
-    end
+    gunicorn_command = new_resource.virtualenv.nil? ? "gunicorn" : "#{::File.join(new_resource.virtualenv, "bin", "gunicorn")}"
+    base_command = "#{gunicorn_command} #{new_resource.app_module}"
     command "#{base_command} -c #{new_resource.application.path}/shared/gunicorn_config.py"
     directory new_resource.directory.nil? ? ::File.join(new_resource.path, "current") : new_resource.directory
     autostart new_resource.autostart
